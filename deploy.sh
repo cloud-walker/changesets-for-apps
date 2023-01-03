f92a87ed3662835a10fd5e6e9758267faf88e2f2
Luca Barone <baro.luc@gmail.com>
2023-01-03 20:37:56 +0100

    wip

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -e
+# set -e
 # set -x
 
 ENV=${1:?"ENV (1st param) is missing"}
@@ -7,28 +7,32 @@ NAME=$(jq -r .name package.json)
 VERSION=$(jq -r .version package.json)
 HASH=$(git rev-parse --short HEAD)
 
+image_exists()
+{
+  return $(docker image inspect $1 > /dev/null ; echo $?)
+}
+
 if [ "$ENV" = "staging" ]; then
-  TAG=lucabarone/changesets-for-apps:$VERSION-$HASH
-  EXISTS=$(docker manifest inspect $TAG > /dev/null; echo $?)
+  IMAGE=lucabarone/changesets-for-apps:$VERSION-$HASH
+  echo "IMAGE: $IMAGE"
   
-  if [ $EXISTS -eq 0 ]; then
-    echo "image $TAG already exists in the registry, skipping."
+  if image_exists $IMAGE; then
+    echo "image $IMAGE already exists in the registry, skipping."
   else
-    echo "image $TAG not found in the registry, pushing..."
-    docker build --tag "$TAG" .
-    docker push "$TAG"
+    echo "image $IMAGE not found in the registry, pushing..."
+    docker build --tag "$IMAGE" .
+    docker push "$IMAGE"
   fi;
 fi;
 
 if [ "$ENV" = "production" ]; then
-  TAG=lucabarone/changesets-for-apps:$VERSION
-  EXISTS=$(docker manifest inspect $TAG > /dev/null; echo $?)
+  IMAGE=lucabarone/changesets-for-apps:$VERSION
 
-  if [ $EXISTS -eq 0 ]; then
-    echo "image $TAG already exists in the registry, skipping."
+  if image_exists $IMAGE; then
+    echo "image $IMAGE already exists in the registry, skipping."
   else
-    echo "image $TAG not found in the registry, pushing..."
-    docker build --tag "$TAG" .
-    docker push "$TAG"
+    echo "image $IMAGE not found in the registry, pushing..."
+    docker build --tag "$IMAGE" .
+    docker push "$IMAGE"
   fi;
 fi;
